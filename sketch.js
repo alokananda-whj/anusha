6fad22e807eb8c216abaac3073a9b9a9b7775647
alokananda.cborty@gmail.com <alokananda.cborty@gmail.com>
2021-07-12 17:45:16 +0530

    new piggy

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -26,7 +25,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+    pig4 = new Pig(300,300);
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -48,7 +47,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    pig4.display()
     box3.display();
     box4.display();
     pig3.display();
